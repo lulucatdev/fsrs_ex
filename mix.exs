664e58a8363383b56a8a028a9f8feefb0f423672
Gaoge Zhang <lucas@lulucat.com>
2026-02-08 02:41:16 +0800

    Add ExDoc dependency for Hex docs publishing

--- mix.exs
+++ mix.exs
@@ -55,7 +55,8 @@ defmodule Fsrs.MixProject do
 
   defp deps do
     [
-      {:jason, "~> 1.4"}
+      {:jason, "~> 1.4"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 end
